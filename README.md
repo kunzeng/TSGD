d1df116a46faa1bff299bed1076a0472b4a0dad0
kunzeng <zki@163.com>
2021-09-06 19:56:35 +0800

    update

--- README.md
+++ README.md
@@ -1,29 +1,26 @@
-## TAdam
+## TSGD
 
 The Pytorch implementation of TSGD algorithm in：'Scaling transition from SGDM to plain SGD'
-[https://arxiv.org/abs/2106.06749](https://arxiv.org/abs/2106.06753)
+[https://arxiv.org/abs/2106.06749](https://arxiv.org/abs/2106.06753)  
+The implementation is highly based on projects [AdaBound](https://github.com/Luolc/AdaBound) , [Adam](https://github.com/pytorch/pytorch/blob/v1.4.0/torch/optim/adam.py) , [pytorch-cifar](https://github.com/kuangliu/pytorch-cifar), thanks pretty work.  
+The test environment we passed is: PyTorch=1.7.0, Python=3.7.10, Linux/Centos8.3.  
 
 ### Usage
+
+Please directly download the [TSGD](https://github.com/kunzeng/TSGD/tree/main/tsgd) folder and put it in your project, then
 
 ```python
 from tsgd import TSGD
 
 ...
 
-optimizer = TSGD(model.parameters(), iters=required, momentum=0.9, lr=1e-3, moment=3/8, up_lr=0.1, low_lr=0.005)
+optimizer = TSGD(model.parameters(), iters=required)
 
 #iters(int, required): iterations
 #	iters = (testSampleSize / batchSize) * epoch
-#
-#moment(float, optional): transition moment
-#       moment = transition_iters / iters
-
-#set default value: momentum=0.9, moment=3/8, up_lr=0.1, low_lr=0.005
 ```
 
 
 
-
-
-The code will be uploaded as soon as possible
+The code will be uploaded as soon as possible.
 
--- tsgd/tsgd.py
+++ tsgd/tsgd.py
@@ -1,4 +1,5 @@
 import torch
+import math
 from torch.optim.optimizer import Optimizer, required
 
 
@@ -15,39 +16,31 @@ class TSGD(Optimizer):
         params (iterable): iterable of parameters to optimize or dicts defining
             parameter groups
         iters(int, required): iterations
-            iters = (testSampleSize / batchSize) * epoch      
+            iters = math.ceil(trainSampleSize / batchSize) * epochs
         lr (float): learning rate 
         momentum (float, optional): momentum factor (default: 0)
         weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
         dampening (float, optional): dampening for momentum (default: 0)
         nesterov (bool, optional): enables Nesterov momentum (default: False)
-        moment(float, optional): transition moment, moment = transition_iters / iters
-        up_lr(float, optional): upper learning rate
-        low_lr(float, optional): lower learning rate         
-        
+        coeff(float, optional): scaling coefficient
+       
     """
 
     def __init__(self, params, lr=required, iters=required, momentum=0.9, 
-                 dampening=0, weight_decay=0, nesterov=False, moment=3/8, up_lr=0.1, low_lr=0.005):
+                 dampening=0, weight_decay=0, nesterov=False, coeff=1e-4):
         if lr is not required and lr < 0.0:
             raise ValueError("Invalid learning rate: {}".format(lr))
-        if not 1.0 <= iters:
+        if not 1 <= iters:
             raise ValueError("Invalid iters: {}".format(iters))             
         if momentum <= 0.0:
             raise ValueError("Invalid momentum value: {}".format(momentum))
         if weight_decay < 0.0:
             raise ValueError("Invalid weight_decay value: {}".format(weight_decay))
-        if not 0.0 < moment < 1:
-            raise ValueError("Invalid gamma: {}".format(moment))                    
-        if not 0.0 <= up_lr:
-            raise ValueError("Invalid up learning rate: {}".format(up_lr))
-        if not 0.0 <= low_lr:
-            raise ValueError("Invalid low learning rate: {}".format(low_lr))            
-        if not low_lr <= up_lr:
-            raise ValueError("required up_lr  >= low_lr, but (up_lr = {}, low_lr = {})".format(up_lr, low_lr))  
+        if not 0.0 <= coeff <= 1.0:
+            raise ValueError("Invalid coeff: {}".format(coeff))                    
             
         defaults = dict(lr=lr, iters=iters, momentum=momentum, dampening=dampening, weight_decay=weight_decay,
-                        nesterov=nesterov, moment=moment, up_lr=up_lr, low_lr=low_lr)
+                        nesterov=nesterov, coeff=coeff)
         if nesterov and (momentum <= 0 or dampening != 0):
             raise ValueError("Nesterov momentum requires a momentum and zero dampening")
             
@@ -59,7 +52,7 @@ class TSGD(Optimizer):
         for group in self.param_groups:
             group.setdefault('nesterov', False)
 
-    def step(self,closure=None):
+    def step(self, epoch, closure=None):
         """Performs a single optimization step.
 
         Arguments:
@@ -71,15 +64,14 @@ class TSGD(Optimizer):
         if closure is not None:
             loss = closure()
         for group in self.param_groups:
-
         
             weight_decay = group['weight_decay']
             momentum = group['momentum']
             dampening = group['dampening']
             nesterov = group['nesterov']
+            coeff = group['coeff']
             iters = group['iters']
-            moment  = group['moment']
-            gamma = 10 ** (-2 / (iters * moment))
+            rho = 10 ** (math.log(coeff, 10) / iters)
 
             for p in group['params']:
                 if p.grad is None:
@@ -102,10 +94,8 @@ class TSGD(Optimizer):
                         d_p = d_p.add(momentum, buf)
                     
                 #Scaling the momentum
-                d_p = (buf - d_p) * (gamma ** param_state['step']) + d_p
-                #Decreasing the learning rate
-                dlr = (group['up_lr'] - group['low_lr']) * (1 - 1/iters * param_state['step']) + group['low_lr']    
+                d_p = (buf - d_p) * (rho ** param_state['step']) + d_p   
                 
-                p.data.add_(d_p, alpha=-dlr)
+                p.data.add_(d_p, alpha=-group['lr'])
                 
         return loss
